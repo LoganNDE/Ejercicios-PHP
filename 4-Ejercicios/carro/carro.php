dc1874be873e520f4b3c2b990ea77946ea98d1b7
Logan <logannaranjo4@gmail.com>
2024-10-19 20:42:06 +0200

    Carro

--- 4-Ejercicios/carro/carro.php
+++ 4-Ejercicios/carro/carro.php
@@ -9,8 +9,6 @@
     
     <?php
         session_start();
-        $_SESSION['totalCarro'] = 0;
-
         $articulos = array(
             array("id" => 1, "nombre" => "Zapatillas Nike", "precio" => 60),
             array("id" => 2, "nombre" => "Sudadera Domyos", "precio" => 15),
@@ -18,6 +16,29 @@
             array("id" => 4, "nombre" => "Pelota de baloncesto Molten", "precio"
             => 20)
             );
+
+        
+        if (($_SERVER['REQUEST_METHOD'] === 'GET') && (isset($_GET['name']))){
+            
+            echo $_GET['name'];
+        if (!isset($_SESSION['carroTotal']) && !isset($_SESSION['precioTotal'])){
+                $_SESSION['carroTotal'] = [];
+                $_SESSION['precioTotal'] = 0;    
+            }
+
+            //SUMA DE LOS PRECIOS DE ARTICULOS
+            foreach ($articulos as $articulo){
+                if ($articulo['id'] == $_GET['name']){
+                    array_push($_SESSION['carroTotal'], $articulo) ;
+                    $_SESSION['precioTotal'] += $articulo['precio'];
+                }
+            }
+
+            print_r($_SESSION['carroTotal']);
+        
+        }
+            echo "<br>". $_SESSION['precioTotal'];
+
     ?>    
 
 
@@ -25,23 +46,11 @@
 
         <ul>
             <?php
-                $lisID = [];
                 foreach ($articulos as $values){
                     echo "<a href='carro.php?name=" . $values['id'] . "'><li>" . $values['nombre'] . " " . $values['precio'] . "</li></a>";
-                    array_push($lisID, $values['id']);
                 }
 
-            
-                if (($_SERVER['REQUEST_METHOD'] === 'GET') && (isset($_GET['name']))){
-                    echo $_GET['name'];
-                }
-
-
-            
-            ?>
-
-
-
+        ?>
 
         </ul>
     </div>
